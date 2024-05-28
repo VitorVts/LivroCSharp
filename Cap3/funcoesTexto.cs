c899489835094798e84a88224916b9264363545f
VitorVts <vitorgguimaraes56@gmail.com>
2024-05-28 13:31:46 -0300

    Aprendido Novas Funções Internas do C#

--- Cap3/funcoesTexto.cs
+++ Cap3/funcoesTexto.cs
@@ -49,7 +49,74 @@ namespace LIVROCSHARP
             else
                 WriteLine("3- nomes diferentes");
 
+            //Remove 
+            WriteLine("Remove - extrai x caracteres a partir da esquerda da expressaão");
+            WriteLine($"texto esquerdo:{empresa.Remove(9)}");
+
+            WriteLine("Captura apenas o primeiro nome das pessoas");
+            string[] nomes = {"Fabricio dos Santos","José da Silva","Roberta Brasil"};
+                foreach(var n in nomes)
+                {
+                    WriteLine($"{n.Remove(n.IndexOf(" "))}");
+                }
+
+            //Replace
+            WriteLine("Replace- troca o conteúdo da expressão");
+            WriteLine($"textoatual: {empresa}");
+
+                var novaEmpresa = empresa.Replace("Microsoft","Google");
+            WriteLine($"texto trocado: {novaEmpresa}");
+
+
+            //Split 
+            WriteLine("Split - divide e extrai cada palavra em um array");
             
+            string NivelLivro = "Este livro é básico de C#.";
+            string[] blocos = NivelLivro.Split(" ");
+            
+            var contador = 1;
+            
+            foreach(var exp in blocos)
+            {
+                WriteLine($"texto{contador++}:{exp}");
+            }
+
+            WriteLine($"Qtde de palavras: {blocos.Count()}");
+
+
+            //Substring 
+            WriteLine("Substringé usado para extrair parte do texto");
+            WriteLine(NivelLivro.Substring(5,14));
+
+            string[] cesta = {"5 Laranjas","10 Goiabas vermelhas","5 pêssegos doces", "5 Bananas"};
+            int qtde = 0;
+
+            foreach(var p in cesta )
+            {
+                //p.IndexOf(" ")+1 retorna a posição inicial logo após o número 
+                //ex: 5 Laranjas = posição 1+1 =2 
+
+                WriteLine($"{p.Substring(p.IndexOf(" ")+1)}");
+
+                //Ler apenas os números para somar na variável qtde 
+                qtde += Convert.ToInt32(p.Substring(0,p.IndexOf(" ")));
+                
+                
+            }
+            WriteLine($"Qtde total : {qtde:n0}");
+
+            //IsNullOrEMpty 
+
+            WriteLine("isNullOrEmpty verifica se a string estpa nula ou vazia");
+            string nome = "Renato";
+            string sobrenome = null;
+                if (!String.IsNullOrEmpty(nome) && !String.IsNullOrEmpty(sobrenome))
+                {
+                    WriteLine($"Nome completo:{nome}{sobrenome}");
+                }
+                else{
+                    WriteLine($"Nome: {nome}");
+                }
         }
     }
 }
