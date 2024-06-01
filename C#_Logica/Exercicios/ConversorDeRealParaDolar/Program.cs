fcd2d6adfb9834d7af691f29a4dd3e3ae3d37735
VitorVts <vitorgguimaraes56@gmail.com>
2024-05-31 22:44:22 -0300

    Feito alguns exericios de Lógica

---
+++ C#_Logica/Exercicios/ConversorDeRealParaDolar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeMoeda
+{
+    public class ConversorDeMoeda
+    {
+        public static void Main(string[]args)
+        {
+            double cotacao,dolar,dolarToReal;
+
+
+            System.Console.WriteLine("-----Conversor de Dolar Real------");
+            Console.Write("Digite a cotação do dolar de Hoje: ");
+            cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor em dolares a ser convertido em Real: ");
+            dolar = double.Parse(Console.ReadLine());
+            dolarToReal = dolar*cotacao;
+            Console.WriteLine($"O valor dos dolares em reais é : {dolarToReal}");
+
+        }
+    }
+}
---
+++ C#_Logica/Exercicios/PagamentoComissao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PagamentoComissao
+{
+    public class PagamentoComissao
+    {
+        public static void Main(string[] args)
+        {
+            int id,codigoPeca,qtnVenda;
+            double precoPeca,valorComissao;
+
+            Console.WriteLine("-----Pagamento Comissões-----");
+            Console.Write("Digite seu ID de Vendedor: ");
+            id = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Código da Peça: ");
+            codigoPeca = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor unitário da peça: ");
+            precoPeca = double.Parse(Console.ReadLine());
+            Console.Write("Digite a Quatidade de peças vendidas: ");
+            qtnVenda = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            double totalVendas = precoPeca * qtnVenda;
+            valorComissao = totalVendas * 0.05;
+
+            Console.WriteLine($"O valor comissão para o vendedor de ID: {id} é {valorComissao}");
+
+
+        }
+    }
+}
