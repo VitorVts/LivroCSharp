abd67ce670dc5cc1d1c410a227c232324ede076c
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-06 18:11:34 -0300

    Criado um programa que calcula o peso ideal de acordo com o sexo do usuario

---
+++ C#_Logica/Exercicios/25PesoIdeal/Program.cs
@@ -0,0 +1,25 @@
+﻿double h ;
+string sexo;
+double pesoIdealH,pesoIdealM;
+
+
+
+
+System.Console.WriteLine("Verifique seu pedo ideal:");
+System.Console.WriteLine();
+System.Console.Write("Para prosseguir informe se você é homem digite h e caso seja mulher M : ");
+sexo = Console.ReadLine();
+System.Console.Write("Informe Sua altura assim como este exemplo 1.80:");
+h = double.Parse(Console.ReadLine());
+
+pesoIdealH = (72.7*h)-58;
+pesoIdealM = (62.1*h)-44.7;
+switch (sexo)
+{
+    case "h":
+    System.Console.WriteLine($"{pesoIdealH}");
+    break;
+    case "m":
+    System.Console.WriteLine($"{pesoIdealM}");
+    break;
+}
