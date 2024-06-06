e6f87220f00930b47f98f83eaced589502063858
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-06 18:09:33 -0300

    Criada uma calculadora entre 2 numeros usando switch case

---
+++ C#_Logica/Exercicios/23Calculadora/Program.cs
@@ -0,0 +1,69 @@
+﻿int a,b;
+string option;
+
+
+
+
+while (true)
+{
+    Console.WriteLine("-----Calculadora------");
+    Console.WriteLine("Digite a operação desejada : \n1-Adição\n2-subtração\n3-Multiplicação\n4-Divisão\n5-Sair");
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+        Console.WriteLine("-----Adição-----");
+        Console.WriteLine();
+        Console.Write("Digite o primeiro valor: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Resultado:  {a + b} ");
+        Console.WriteLine();
+        break;
+
+        case "2":
+        Console.WriteLine("-----Subtração-----");
+        Console.WriteLine();
+        Console.Write("Digite o primeiro valor: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Resultado:  {a - b} ");
+        Console.WriteLine();
+        break;
+        
+        case "3":
+        Console.WriteLine("-----Multiplicação-----");
+        Console.WriteLine();
+        Console.Write("Digite o primeiro valor: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Resultado:  {a * b} ");
+        Console.WriteLine();
+        break;
+
+        case "4":
+        Console.WriteLine("-----Divisão-----");
+        Console.WriteLine();
+        Console.Write("Digite o primeiro valor: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Resultado:  {a / b} ");
+        Console.WriteLine();
+        break;
+        
+        
+    }
+    
+    Console.WriteLine("Se deseja sair digite 5 caso não digite a opção de acordo  para realizar outra operação :");
+    Console.WriteLine();
+
+    if(option == "5")
+    {
+        break;
+    }
+}
