bdb36cc2edd0e691b456b63c4ec2995fabe5091f
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-07 08:56:12 -0300

    Criado um programa que gera a tabuada com o número que o usuario insere

---
+++ C#_Logica/Exercicios/30-Tabuada/Program.cs
@@ -0,0 +1,13 @@
+﻿int tabuada;
+
+System.Console.WriteLine("----Gerador de Tabuada-----");
+System.Console.WriteLine();
+System.Console.Write("Digte o numero que deseja criar a tabuada: ");
+tabuada = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Aqui está a sua tabuada de {tabuada}");
+
+for(int i = 0;i<=10;i++)
+{
+    System.Console.WriteLine($"{tabuada}x{i}={tabuada*i}");
+}
