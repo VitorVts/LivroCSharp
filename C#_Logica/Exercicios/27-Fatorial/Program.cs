567161044b86e7d293843a76c944854165411c5d
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-07 08:53:36 -0300

    Criado um programa que calcula o fatorial de um numero inserido pelo usuario

---
+++ C#_Logica/Exercicios/27-Fatorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para efetuar o fatorial: ");
+        int n = int.Parse(Console.ReadLine());
+
+        long fatorial = Fatorial(n);
+
+        Console.WriteLine($"O fatorial de {n} é {fatorial}");
+    }
+
+    static long Fatorial(int n)
+    {
+        long resultado = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
