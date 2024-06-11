4e912f2443efd3657fb85190e26649afbef29ff7
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-10 23:15:35 -0300

    Feito exercicio de estrutura de repetição mais condicionais

---
+++ C#_Logica/Exercicios/32/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Digite um número inteiro positivo menor que 10 e maior que zero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero <= 0 || numero >= 10)
+        {
+            Console.WriteLine("Número inválido. Por favor, insira um número menor que 10 e maior que zero.");
+            return;
+        }
+
+        int contador = 0;
+        int somaQuadrados = 0;
+
+        for (int i = numero; contador < 20; i++)
+        {
+            if (i % 2 != 0)
+            {
+                somaQuadrados += i * i;
+                contador++;
+            }
+        }
+
+        Console.WriteLine("A soma dos quadrados dos 20 primeiros números ímpares a partir de " + numero + " é: " + somaQuadrados);
