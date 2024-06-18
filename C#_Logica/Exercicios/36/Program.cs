29c5ef22e9d08a958cb15114ce108483e513a117
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-17 23:09:15 -0300

    Feito Mais exercicios referente a estrutura de repetição em c#

---
+++ C#_Logica/Exercicios/36/Program.cs
@@ -0,0 +1,29 @@
+﻿int menorValor = int.MaxValue;
+int maiorValor = int.MinValue;
+int soma = 0;
+
+for(int i = 0; i < 10; i++)
+{
+    System.Console.Write("Digite um número: ");
+    int valorLido = int.Parse(Console.ReadLine()); 
+
+    //if(valorLido > maiorValor)
+    //{
+     //   maiorValor = valorLido;
+    //}
+    //if(valorLido < menorValor)
+    //{
+     //   menorValor = valorLido;
+    //}
+
+    maiorValor = (valorLido>maiorValor) ? valorLido : maiorValor;
+    menorValor = (valorLido<menorValor) ? valorLido : menorValor;
+
+
+
+    soma += valorLido;
+}
+
+double media = soma / 10.0;
+
+System.Console.WriteLine($"Maior: {maiorValor}, Menor: {menorValor}, Média: {media}");
