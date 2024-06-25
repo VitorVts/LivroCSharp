3ff084ead310d7f62ad19b3d88fc8f3d920bdfd8
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-25 08:23:13 -0300

    Criado um programa para calculo de hora extra de funcionários

---
+++ C#_Logica/Exercicios/38/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ContaSálario
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int codigo = 0;
+            int numero = 0;
+            double horastrab = 10.0;
+            double extra = 0.0;
+            double horaExtra = 0.0;
+            string option = "";
+            do
+            {
+                Console.Clear();
+                System.Console.WriteLine("Digite seu código de Funcionário:");
+                codigo = int.Parse(Console.ReadLine());
+                System.Console.WriteLine("Digite Suas horas trabalhadas:");
+                numero = int.Parse(Console.ReadLine());
+
+                double salario = numero * horastrab;
+
+                if (numero > 50)
+                {
+                    extra = numero - 50;
+                    horaExtra = extra * 20.0;
+                    salario = (50 * horastrab) + horaExtra;
+                    Console.WriteLine($"Olá {codigo}, seu salário total é: R${salario}");
+                    Console.WriteLine($"E o total de extras é: R${horaExtra}");
+                }
+                else
+                {
+                    extra = 0;
+                    horaExtra = 0;
+                    Console.WriteLine($"\nOlá {codigo}, seu salário total é: R${salario}");
+                    Console.WriteLine("E não há salário extra.");
+                }
+                Console.WriteLine("\nDigite S para sair ou qualquer tecla para rodar novamente");
+                option = Console.ReadLine().ToUpper();
+            } while (option != "S");
+        }
+    }
+}
