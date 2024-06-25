e835792e6623fa24ecb3809ea657202af9243441
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-25 09:03:05 -0300

    Criado um Programa para validação se um número é par impar positivo ou negativo

---
+++ C#_Logica/Exercicios/39/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ParOuImpar
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numero;
+            string option;
+
+            do
+            {
+                Console.Clear();
+                System.Console.Write("Digite Um Número para verificar se é Par ou Impar: ");
+                bool eNumero = int.TryParse(Console.ReadLine(), out numero);
+
+                if (eNumero)
+                {
+                    if (numero % 2 == 0)
+                        System.Console.WriteLine($"O valor de {numero} é par");
+                    else
+                        System.Console.WriteLine($"O valor de {numero} é impar");
+
+                    if (numero >= 0)
+                        Console.WriteLine($"O valor de {numero} é positivo");
+                    else
+                        Console.WriteLine($"O valor de {numero} é negativo");
+                }
+                else
+                {
+                    Console.WriteLine("\nEntrada inválida. Por favor, digite um número inteiro.\n");
+                }
+                System.Console.WriteLine("Deseja Sair do Programa? Digite S");
+                option = Console.ReadLine().ToUpper();
+            } while (option != "S");
+        }
+    }
+}
