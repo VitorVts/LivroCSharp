0266927a89f80574f72822efd3dc8d2d92430fa6
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-25 12:09:08 -0300

    Criado um programa que classifica de acordo com a idade

---
+++ C#_Logica/Exercicios/41/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AgeClassifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string option;
+            int idade;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite sua idade:");
+
+                bool eNumero = !int.TryParse(Console.ReadLine(), out idade);
+
+                if (eNumero || idade < 0)
+                {
+                    Console.WriteLine("Por favor, digite uma idade válida.");
+                }
+                else
+                {
+                    if (idade >= 18)
+                    {
+                        Console.WriteLine("Categoria: Adulto");
+                    }
+                    else if (idade >= 14)
+                    {
+                        Console.WriteLine("Categoria: Juvenil B");
+                    }
+                    else if (idade >= 12)
+                    {
+                        Console.WriteLine("Categoria: Juvenil A");
+                    }
+                    else if (idade >= 8)
+                    {
+                        Console.WriteLine("Categoria: Infantil B");
+                    }
+                    else if (idade >= 5)
+                    {
+                        Console.WriteLine("Categoria: Infantil A");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Idade fora das categorias especificadas.");
+                    }
+                }
+
+                Console.WriteLine(
+                    "\nDeseja encerrar o programa? Digite S para sair ou qualquer outra tecla para continuar."
+                );
+                option = Console.ReadLine().ToUpper();
+            } while (option != "S");
+        }
+    }
+}
